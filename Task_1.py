059b021e422f87748354f4ce7e383ac3ce24fa3c
Sourish-18 <sourishmk18@gmail.com>
2025-12-05 20:07:34 +0530

    initial commit

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+num_1 = int (input ("Enter the first number: "))
+num_2 = int (input ("Enter the second number: "))
+
+add = num_1 + num_2
+sub = num_1 - num_2
+multiply = num_1 * num_2
+div = num_1 / num_2
+
+print ("\nAddition: ", add)
+print ("Subtraction: ", sub)
+print ("Mulitplication: ", multiply)
+print ("Division: ", div)
